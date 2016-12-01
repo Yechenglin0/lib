bb4dcce72e3fc53eb9fc551cc2c031cb6c1e3511
YeChenglin0 <993912136@qq.com>
2016-12-01 18:38:15 +0800

    最近图书 去重

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -253,7 +253,7 @@ class IndexController extends Controller {
             ));
         }
 
-        $info = M('t_ts')->field("tm,tstm,zrz,gcdmc")->order("rdrq desc")->group('tm,tstm,zrz,gcdmc')->limit($num)->select();
+        $info = M('t_ts')->field("tm,tstm,zrz,gcdmc")->order("rdrq desc")->group('tm')->limit(0,$num)->select();
         $i = 0;
         foreach ($info as $var) {
             $data[$i]['bookName'] = $var['TM'];                   //图书名称
